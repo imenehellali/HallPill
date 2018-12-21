35dea18bbeb3d0b31760575fdc6c7af2b43109d7
hellali <hellali_imene@yahoo.fr>
2018-12-21 18:54:20 +0100

    added MainMenu Scene basic things no functionalities yet and added sounds

---
+++ Assets/Scripts/MainMenuScripts/SkyMvT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyMvT : MonoBehaviour {
+    public float _rotationSpeed;
+
+	void Update () {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * _rotationSpeed);
+
+	}
+}
