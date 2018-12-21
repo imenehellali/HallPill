511b2f8a7f2c8e96cb0b01d6bafa507ed6a5db3f
hellali <hellali_imene@yahoo.fr>
2018-12-21 23:05:12 +0100

    completed the whole design of main menu scene next time i will do the functionalities and the scripts related to it and that will be it for my task until i finish the monster/ the opponent

---
+++ Assets/PillSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillSpawn : MonoBehaviour {
+    public float _spawnRate;
+    private float _randNum;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _randNum = Random.Range(-100, 100);
+
+	}
+    
+}
